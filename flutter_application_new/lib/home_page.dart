c4766dac00dc24d160fb4b6a78302d6396ce6f39
Hiruni Navoda <shhiruninawoda@gmail.com>
2023-07-11 20:24:31 +0530

    new

---
+++ flutter_application_new/lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required String title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child: Text('Hi'),
+        ));
+  }
+}
